4127cd410c10a7443bf0db37210e38de989894e4
ARUN VIGNESH <a.s.arunvignesh@gmail.com>
2020-08-05 23:43:19 +0530

    Completed

--- blogger/src/controllers/index_controller.rb
+++ blogger/src/controllers/index_controller.rb
@@ -1,10 +1,16 @@
 require_relative './../app'
+require_relative './../sitemap'
 require 'sinatra/base'
 
 module Shivin
   class IndexController < App
     get "/" do
       slim :index, {locals: {settings_db: settings.db, port: 2230}}
+    end
+
+    get "/generate-sitemap" do
+      create_sitemap
+      "success"
     end
   end
 end
---
+++ blogger/src/sitemap.rb
@@ -0,0 +1,21 @@
+require 'xml-sitemap'
+
+def create_sitemap
+  map = XmlSitemap::Map.new('arun-tech-blogs.herokuapp.com',:secure => true) do |m|
+    # Adds a simple page
+    m.add '/', :priority => 0.5, :updated => Date.today, :period => :weekly
+
+    # You can drop leading slash, it will be automatically added
+    m.add '/blog/solid-principles' , :priority => 0.9, :updated => Date.new(2020,07,30), :period => :weekly
+
+    # Set the page priority
+    m.add '/about', :priority => 0.6, :updated => Date.new(2020,07,30), :period => :weekly
+
+  end
+
+  # Render the sitemap XML
+  map.render
+
+  # Render and save XML to the output file
+  map.render_to('./public/site-map.xml')
+end
